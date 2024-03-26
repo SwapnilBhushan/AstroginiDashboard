f57b8060433e39cf93584c68da6191dd869562bf
Swapnil Bhushan <swapnilbhushan2020@gmail.com>
2024-03-26 23:12:24 +0530

    url changed

--- src/Constant.js
+++ src/Constant.js
@@ -1,4 +1,4 @@
-const Service_URL = "http://16.16.192.116:3000";
-//const Service_URL = "http://localhost:3000";
+//const Service_URL = "http://16.16.192.116:3000";
+const Service_URL = "http://localhost:3000";
 
 export default Service_URL;
